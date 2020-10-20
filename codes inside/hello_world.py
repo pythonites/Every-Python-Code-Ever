42f1758f3315570118a3364e32f9b373d234d4b0
pythonites <73133008+pythonites@users.noreply.github.com>
2020-10-20 10:18:19 +0530

    Create hello_world.py

---
+++ codes inside/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+"""
+ print() is an inbuilt funtion that prints 
+ the specified message on the output screen.  
+ 
+ Syntax:
+     print(object(s), sep=separator, end=end, file=file, flush=flush)
+"""
