e0ac6da51cfcb308bbfee56086718bd2de949465
coderAbhii <62416129+coderAbhii@users.noreply.github.com>
2020-10-20 10:49:31 +0530

    Update stopwatch.py

--- codes inside/stopwatch.py
+++ codes inside/stopwatch.py
@@ -17,3 +17,34 @@ else:
 	
 print('time is', l-s)
 
+import time #import the required library
+
+#Starting the timer
+
+start = input('Enter any key to start: ')
+
+if start == 1:
+
+	s = time.time() #records the time
+
+else:
+
+	s = time.time()
+
+	
+
+#stop the timer
+
+end = input('Press any key to end..')
+
+if end == 1 :
+
+	l = time.time()
+
+else:
+
+	l = time.time()
+
+	
+
+print('Time is', l-s)
