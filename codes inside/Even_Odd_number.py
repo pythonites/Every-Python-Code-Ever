a542720e4da3c8919b6a171220924c664e7a893c
pythonites <73133008+pythonites@users.noreply.github.com>
2020-10-23 16:23:17 +0530

    created even_odd_number.py

---
+++ codes inside/Even_Odd_number.py
@@ -0,0 +1,17 @@
+#PROGRAM TO FIND IF NUMBER IS EVEN OR ODD
+
+num = int(input("Enter any number: "))
+
+if (num % 2 == 0):
+	print(f"{num} is an EVEN number")
+
+else:
+	print(f"{num} is an ODD number")
+	
+	
+"""
+ points to look for:
+ 	1. input() function
+ 	2. if-else statement
+ 	3. f- strings (in line 6 and line 9)
+"""
