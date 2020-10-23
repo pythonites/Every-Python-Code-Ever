3af7b54e07e8c2ec8e96f2b62b5bc5eff30ebd2e
pythonites <73133008+pythonites@users.noreply.github.com>
2020-10-23 17:13:08 +0530

    Create Star_pattern_1.py

---
+++ codes inside/Star_pattern_1.py
@@ -0,0 +1,29 @@
+#PROGRAM TO PRINT TRIANGULAR STAR PATTERN 
+
+row = int(input("Enter number of rows: "))
+
+for i in range(1,row+1):
+	print("*"*i)
+	
+'''
+OUTPUT:
+	Enter number of rows: 10
+	*
+	**
+	***
+	****
+	*****
+	******
+	*******
+	********
+	*********
+	**********
+	
+'''
+
+"""
+Points to look for:
+	1. for loop
+	2. 'in' keyword
+	3  range() function
+"""
