b92a351b9ebf69cf7c48d60685898cb0aacc497d
pythonites <73133008+pythonites@users.noreply.github.com>
2020-10-23 18:24:08 +0530

    Create all_python_keywords.py

---
+++ codes inside/all_python_keywords.py
@@ -0,0 +1,46 @@
+import keyword
+
+keywords = keyword.kwlist
+
+for keyword in keywords:
+	print(keyword)
+
+"""
+OUTPUT:
+False
+None
+True
+and
+as
+assert
+async
+await
+break
+class
+continue
+def
+del
+elif
+else
+except
+finally
+for
+from
+global
+if
+import
+in
+is
+lambda
+nonlocal
+not
+or
+pass
+raise
+return
+try
+while
+with
+yield
+
+"""
