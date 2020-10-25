b073a12163ad00203f859b70582f71c37d8ec713
pythonites <73133008+pythonites@users.noreply.github.com>
2020-10-25 22:11:18 +0530

    Create quick_date_time.py

---
+++ codes inside/quick_date_time.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+today = datetime.now()
+today = today.strftime("%d/%m/%Y %H:%M:%S")
+
+print(today)
+
+"""
+Points to ponder:
+	1. from keyword
+	2. import keyword
+	3. now() function 
+	4. strftime() function
+"""
