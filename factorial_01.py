2afaf9d765b2db8ac582b68cd5f9d975de67a35b
Pratik Bambulkar <72552513+Pratik180198@users.noreply.github.com>
2020-10-25 20:19:54 +0530

    added factorial program using for loop

---
+++ factorial_01.py
@@ -0,0 +1,10 @@
+num1=input("Enter number: ")
+try:
+    num=int(num1)
+    fact=1
+    for i in range(1,num+1): #Using For Loop
+        fact=fact*i
+    print("Factorial of {} is {}".format(num,fact)) #For Loop Answer
+except:
+    print("Please enter only integer")
+ 
